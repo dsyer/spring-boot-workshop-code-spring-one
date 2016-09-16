27b07fcfd58db1bb64d6aaa8b307168e719688bc
Dave Syer <dsyer@pivotal.io>
2016-09-16 09:48:56 +0100

    Fix OAuth2 configuration

--- 06-security/oauth/oauth-resource-server/src/main/java/io/pivotal/workshop/OAuthResourceServerApplication.java
+++ 06-security/oauth/oauth-resource-server/src/main/java/io/pivotal/workshop/OAuthResourceServerApplication.java
@@ -2,10 +2,13 @@ package io.pivotal.workshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerTokenServicesConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableResourceServer
 @SpringBootApplication
+@Import(ResourceServerTokenServicesConfiguration.class)
 public class OAuthResourceServerApplication {
 
 	public static void main(String[] args) {
